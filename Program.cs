44deee7602cdca2cbfbf20f45bd0aa0e7bbc2daf
SofyaFedorovich <s_fed@list.ru>
2021-10-21 22:26:14 +0300

    Первая задача

--- Program.cs
+++ Program.cs
@@ -10,6 +10,18 @@ namespace Занятие_5
     {
         static void Main(string[] args)
         {
+            // среднее арифметическое массива из семи элементов
+            int[] array = new int[7];
+            int Summa = 0;
+            for (int i = 0; i < 7; i++)
+            {
+                Console.Write("Введите число массива: {0}", i + 1);
+                array[i] = Convert.ToInt32(Console.ReadLine());
+                Summa += array[i];
+            }
+            Console.Write("Среднее арифметическое членов массива: {0}", Summa / 7);
+            Console.ReadKey();
         }
+
     }
 }
